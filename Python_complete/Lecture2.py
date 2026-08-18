fbddcc64af6568d8a16ed9d91ac53d1e466779a1
mishrp13 <prabalsonu1997@gmail.com>
2026-08-18 17:52:13 +0530

    new

---
+++ Python_complete/Lecture2.py
@@ -0,0 +1,7 @@
+spice_mix=set()
+print(f"Initial spice mix id: {id(spice_mix)}")
+spice_mix.add("Ginger")
+spice_mix.add("cardamon")
+print(f"Initial spice mix id: {id(spice_mix)}")
+
+# its mutable
