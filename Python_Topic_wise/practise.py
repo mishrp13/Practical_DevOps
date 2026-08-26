5b6aadd29e173c1590d6231d94aa9648dbacb0a5
mishrp13 <prabalsonu1997@gmail.com>
2026-08-26 18:41:12 +0530

    new

--- Python_Topic_wise/practise.py
+++ Python_Topic_wise/practise.py
@@ -1,5 +1,29 @@
-my_tuple= (1,2) 
-new_tuple=my_tuple + (3.14,'cherry')
+class Solution():
+
+    def reverse_number(self,n):
+
+        copy=n
+
+        reverse_number=0
+
+        while n>0:
+
+            last_digit= n%10
+            reverse_number=(reverse_number*10)+last_digit
+            n=n//10
+
+        return reverse_number == copy
 
 
-print(new_tuple)
+n=121
+sol=Solution()
+ans=sol.reverse_number(n)
+print(f"{ans}")
+
+
+
+
+
+       
+       
+
